6963c4645987f1e21d17fe293020e58695eb82b0
Mohammad Asif Khan <mohammad.asifkhan@go-mmt.com>
2020-08-15 15:40:33 +0530

    simple solution for remove element updated

--- removeElement.go
+++ removeElement.go
@@ -1,39 +1,21 @@
 package main
 
-// func main() {
-// 	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
-// }
+import "fmt"
+
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
+}
 
 func removeElement(nums []int, val int) int {
 	if len(nums) == 0 || (len(nums) == 1 && nums[0] == val) {
 		return 0
 	}
-
-	i := 0
-	for i = 0; i < len(nums); i++ {
-		if nums[i] != val {
-			break
-		}
-	}
-	if i == len(nums) {
-		return 0
-	}
-	i = 0
-	j := len(nums) - 1
-	k := 0
-	var temp int
-	for i = 0; i <= j; i++ {
+	for i := 0; i < len(nums); {
 		if nums[i] == val {
-			k++
-			temp = nums[i]
-			nums[i] = nums[j]
-			nums[j] = temp
-			j--
-			i--
+			nums = append(nums[:i], nums[i+1:]...)
+			continue
 		}
+		i++
 	}
-	if nums[j] == val {
-		k++
-	}
-	return len(nums) - k
+	return len(nums)
 }
